d24a1b948c9939247bc25cc9baf0e3b2092f1a09
Bill Chen <bill@spatial.is>
2019-10-28 12:03:01 -0400

    basic mixpanel integration

---
+++ Assets/Tester.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using mixpanel;
+
+public class Tester : MonoBehaviour
+{
+    public TextMeshPro text;
+    public int count = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mixpanel.Identify("some-random-user");
+    }
+
+    public void OnTrackPress()
+    {
+        count++;
+        text.SetText($"Clicked {count}");
+
+        Value props = new Value();
+        props["count"] = count;
+        Mixpanel.Track("Clicked", props);
+    }
+
+    public void OnFlushPress()
+    {
+        text.SetText("Flush called");
+        Mixpanel.Flush();
+    }
+}
